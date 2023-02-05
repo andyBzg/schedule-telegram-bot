690a752c25fd32bfc1cbf88cab771e7d835a9842
andyBzg <andr.bzg@gmail.com>
2023-02-05 12:42:49 +0100

    correct if-else

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -12,6 +12,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.example.ServiceCommand.*;
+
 public class Bot extends TelegramLongPollingBot {
 
     private final String BOT_NAME = System.getenv("BOT_NAME");
@@ -22,7 +24,7 @@ public class Bot extends TelegramLongPollingBot {
     public Bot() {
         storage = new Storage();
         initCommands();
-        }
+    }
 
 
     @Override
@@ -56,7 +58,8 @@ public class Bot extends TelegramLongPollingBot {
         if (message != null) {
             try {
                 execute(message);
-            } catch (TelegramApiException e) {
+            }
+            catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
@@ -66,18 +69,23 @@ public class Bot extends TelegramLongPollingBot {
 
         String response;
         //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
-        if (ServiceCommand.START.equals(textMsg))
+        if (START.equals(textMsg)) {
             response = greetings();
-        else if (ServiceCommand.HELP.equals(textMsg)) {
+        }
+        else if (HELP.equals(textMsg)) {
             response = info();
-        } else if (ServiceCommand.REQUIRED.equals(textMsg))
+        }
+        else if (REQUIRED.equals(textMsg)) {
             response = storage.showRequiredLectures();
-        else if (ServiceCommand.ELECTIVE.equals(textMsg))
+        }
+        else if (ELECTIVE.equals(textMsg)) {
             response = storage.showElectiveLectures();
-        else if (ServiceCommand.CONSULTATION.equals(textMsg)) {
+        }
+        else if (CONSULTATION.equals(textMsg)) {
             response = storage.showConsultations();
-        } else
-            response = textMsg;
+        }
+        else
+            response = null;
 
         return response;
     }
@@ -85,7 +93,7 @@ public class Bot extends TelegramLongPollingBot {
     private String greetings() {
         return """
                 Привет!\s
-                \nБот умеет показывать расписание лекций и отвечать на сообщение таким же сообщением =)\s
+                \nБот умеет показывать расписание лекций\s
                 \nЧтобы посмотреть список доступных команд воспользуйтесь меню или введите /help""";
     }
 
@@ -101,12 +109,13 @@ public class Bot extends TelegramLongPollingBot {
 
     private void initCommands() {
         List<BotCommand> commandList = new ArrayList<>();
-        commandList.add(new BotCommand(ServiceCommand.START.toString(), "get a welcome message"));
-        commandList.add(new BotCommand(ServiceCommand.REQUIRED.toString(), "get required lectures"));
-        commandList.add(new BotCommand(ServiceCommand.ELECTIVE.toString(), "get elective lectures"));
-        commandList.add(new BotCommand(ServiceCommand.CONSULTATION.toString(), "get consultations"));
-        commandList.add(new BotCommand(ServiceCommand.HELP.toString(), "show all commands"));
-        try{
+        commandList.add(new BotCommand(START.toString(), "get a welcome message"));
+        commandList.add(new BotCommand(REQUIRED.toString(), "get required lectures"));
+        commandList.add(new BotCommand(ELECTIVE.toString(), "get elective lectures"));
+        commandList.add(new BotCommand(CONSULTATION.toString(), "get consultations"));
+        commandList.add(new BotCommand(HELP.toString(), "show all commands"));
+
+        try {
             this.execute(new SetMyCommands(commandList, new BotCommandScopeDefault(), null));
         }
         catch (TelegramApiException e) {
